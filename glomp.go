2d0105c377c7821fdb55f913b8a11097fc884c97
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-01 01:15:10 -0500

    Started config file parsing.

--- glomp.go
+++ glomp.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	config "github.com/kless/goconfig/config"
 	"fmt"
 	"flag"
 	mpd "github.com/jteeuwen/go-pkg-mpd"
 	"os"
+	"strings"
 )
 
+var file = "~/.config/glomp.conf"
 var addr = flag.String("p", ":6615", "Port used by mpd.")
 var pass = flag.String("pass", "", "Password for connecting to mpd.")
 
 func main() {
 	flag.Parse()
+	c, err := config.ReadDefault(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	port, _ := c.String("default", "port")
+	if strings.Index(port, ":") != 0 {
+		port = ":" + port
+	}
+	*addr = port
 	client, err := mpd.Dial(*addr, *pass)
 	if err != nil {
 		fmt.Println(err)
