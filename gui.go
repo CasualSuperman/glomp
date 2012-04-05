7818535fe8d481448e5fab7f561133e1f93d847a
Robert Wertman <robert.wertman@gmail.com>
2012-04-04 22:00:41 -0400

    Switched gtk libs.

--- gui.go
+++ gui.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	gtk "github.com/mattn/go-gtk/gtk"
-	"os"
+	gtk "github.com/norisatir/go-gtk3/gtk"
 )
 
 func showGui(conn int) {
-		gtk.Init(&os.Args)
-		window := gtk.Window(gtk.GTK_WINDOW_TOPLEVEL)
+		gtk.Init()
+		window := gtk.NewWindow(gtk.GtkWindowType.TOPLEVEL)
 		window.SetTitle("glomp")
 		window.Connect("destroy", gtk.MainQuit)
-		window.SetSizeRequest(400, 200)
+		window.SetDefaultSize(400, 200)
 		window.ShowAll()
 
 		gtk.Main()
